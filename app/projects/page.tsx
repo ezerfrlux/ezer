d1b77fb49e4686b8254d3d6c3789084305bbd2ad
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-29 21:53:00 -0600

    [UPDATE] - New projects added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,39 +5,7 @@ import Image from "next/image"
 import LightRays from "../src/components/animations/bg/LightRays"
 import Shuffle from "../src/components/animations/text/Shuffle"
 
-
-const projectsPortfolio = [
-  {
-    id: 1,
-    title: "Cubos Rubik Nicaragua",
-    description:
-      "A responsive rubik's sotre built with Next.js and Tailwind CSS, featuring smooth animations and a clean design.",
-    videoUrl: "",
-    imageUrl: "/img/project1.png",
-    tech: ["Next.js", "Tailwind", "Framer Motion"],
-    link: "https://cubos-rubik-nicaragua-store.vercel.app/",
-  },
-  {
-    id: 2,
-    title: "Online Courses Template",
-    description:
-      "An elegant online courses with shopping car functionality, using tailwind and next js.",
-    imageUrl: "/img/project2.png",
-    videoUrl: "",
-    tech: ["React", "Next.js", "Tailwind"],
-    link: "https://courses-shopping-cart.netlify.app/",
-  },
-  {
-    id: 3,
-    title: "GuitarLA",
-    videoUrl: "",
-    description:
-      "An modern style in this online guitar store using tailwind and next js.",
-    imageUrl: "/img/project3.png",
-    tech: ["React", "Next.js", "CSS modules"],
-    link: "https://guitar-la-nextjs-ecru.vercel.app/",
-  },
-];
+import { projectsPortfolio } from "./projectsPortfolio"
 
 export default function projects() {
   return (
